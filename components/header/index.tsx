da02bd2433194827937b64e64d3b95d7309e65cb
Viktor Skorodumov <skorodumoviktor@gmail.com>
2022-03-25 00:47:36 +0300

    feat

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,7 +1,19 @@
 export function Header() {
   return (
-    <div className="fixed top-0 left-0 right-0 h-[50px]">
-      <div className="container mx-auto px-4">Header</div>
+    <div className="fixed top-0 left-0 right-0 flex flex-row h-[50px] bg-green-200">
+      <div className="container mx-auto px-4 flex flex-row flex items-center justify-between">
+        <div className="flex flex-row">
+          <div className="mr-3">Обо мне</div>
+          <div className="mr-3">Опыт</div>
+          <div className="mr-3">Skills</div>
+          <div>Контакты</div>
+        </div>
+
+        <div className="flex flex-row">
+          <div>PDF</div>
+          <div className="ml-3">Dark Mode</div>
+        </div>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,32 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      green: {
+        100: "#d9ed92ff",
+        200: "#b5e48cff",
+        300: "#99d98cff",
+        400: "#76c893ff",
+        500: "#52b69aff",
+        600: "#34a0a4ff",
+      },
+      blue: {
+        100: "#168aadff",
+        200: "#1a759fff",
+        300: "#1e6091ff",
+        400: "#184e77ff",
+      },
+      dark: {
+        100: "#6a6b70ff",
+        200: "#5b5c62ff",
+        300: "#4b4c52ff",
+        400: "#393a41ff",
+        500: "#2f3037ff",
+        600: "#26262cff",
+        700: "#1c1c21ff",
+        800: "#131316ff",
+      },
+    },
   },
   plugins: [],
 };
